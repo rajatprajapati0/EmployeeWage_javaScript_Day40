9bf12465208b87513385e5630edb1fad96bfa981
Rajat <rajatprajapati09@gmail.com>
2023-03-18 04:03:42 +0530

    refactored code and using function to get empHours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -7,14 +7,18 @@ const wagePerHour=20;
 
 let empHours=0;
 
-let empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck)
+function getWorkingHours(empCheck)
 {
- case isPartTime: empHours=partTimeHour;
- break;
- case isFullTime: empHours=fullTimeHour;
- break;
- default: empHours;
+    switch(empCheck)
+    {
+     case isPartTime: return partTimeHour;
+     
+     case isFullTime: return fullTimeHour;
+     
+     default: return empHours;
+    }
 }
+let empCheck=Math.floor(Math.random()*10)%3;
+empHours=getWorkingHours(empCheck)
 let empWage=empHours*wagePerHour;
 console.log(`Employee Wage:${empWage}$`);
