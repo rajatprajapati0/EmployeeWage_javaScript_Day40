5661d9cd6952aca72da03c8b39521fc34b8c33f1
Rajat <rajatprajapati09@gmail.com>
2023-03-18 04:40:57 +0530

    calWages till Working Days or Ttl Working Hrs permonth is Reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,8 +5,8 @@ const partTimeHour=4;
 const fullTimeHour=8;
 const wagePerHour=20;
 const numberOfWoringDays=20;
-
-
+const maxHoursInMonth=100;
+let totalWorkingDay=0;
 function getWorkingHours(empCheck)
 {
     switch(empCheck)
@@ -18,13 +18,19 @@ function getWorkingHours(empCheck)
      default: return 0;
     }
 }
-let empHours=0;
-for(let day=0;day<numberOfWoringDays;day++)
+let totalEmpHours=0;
+while(totalEmpHours<=maxHoursInMonth&&totalWorkingDay<numberOfWoringDays)
 {
+    totalWorkingDay++;
     let empCheck=Math.floor(Math.random()*10)%3;
-    empHours+=getWorkingHours(empCheck);
+    totalEmpHours+=getWorkingHours(empCheck);
 }
 
 
-let empWage=empHours*wagePerHour;
-console.log(`Employee Wage:${empWage}`);
+let empWage=totalEmpHours*wagePerHour;
+console.log(`
+            Total Hours  :${totalEmpHours}
+            Total Day    :${totalWorkingDay}
+            Employee Wage:${empWage}
+            
+            `);
