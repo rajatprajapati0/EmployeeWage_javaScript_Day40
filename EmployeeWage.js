fd583a8b3b4aa83d834025d161a6b7938e42fa61
Rajat <rajatprajapati09@gmail.com>
2023-03-18 03:56:39 +0530

    using switch case and calculating wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,20 @@
 const isAbsent=0;
+const isPartTime=1;
+const isFullTime=2;
+const partTimeHour=4;
+const fullTimeHour=8;
+const wagePerHour=20;
 
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==isAbsent)
+let empHours=0;
+
+let empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck)
 {
-    console.log('Emplyee is Absent');
-    
+ case isPartTime: empHours=partTimeHour;
+ break;
+ case isFullTime: empHours=fullTimeHour;
+ break;
+ default: empHours;
 }
-else
-{
-    console.log("Employee is Present");
-}
+let empWage=empHours*wagePerHour;
+console.log(`Employee Wage:${empWage}$`);
